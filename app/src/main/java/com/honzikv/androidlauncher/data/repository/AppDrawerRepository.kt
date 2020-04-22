42b73399a8b24ad504410b4358565e73024feb2a
itznukeey <37068553+itznukeey@users.noreply.github.com>
2020-04-22 13:27:12 +0200

    background work

--- app/src/main/java/com/honzikv/androidlauncher/data/repository/AppDrawerRepository.kt
+++ app/src/main/java/com/honzikv/androidlauncher/data/repository/AppDrawerRepository.kt
@@ -1,13 +1,12 @@
 package com.honzikv.androidlauncher.data.repository
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.honzikv.androidlauncher.data.model.DrawerApp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import java.util.*
 
 /**
  * [packageManager] - package manager to obtain all app packages
@@ -24,16 +23,20 @@ class AppDrawerRepository(private val packageManager: PackageManager) {
      * Updates systemApps LiveData with new list of system apps
      */
     suspend fun reloadAppList() = withContext(Dispatchers.Default) {
-        val systemPackages = packageManager.getInstalledApplications(0) ?: listOf()
         val apps = mutableListOf<DrawerApp>()
 
+        //Get all launchable apps
+        val systemPackages = packageManager.queryIntentActivities(
+            Intent(Intent.ACTION_MAIN, null).apply { addCategory(Intent.CATEGORY_LAUNCHER) }, 0
+        )
+
         //Maps each item obtained via package manager to LauncherApp
-        systemPackages.forEach { systemAppInfo ->
+        systemPackages.forEach { resolveInfo ->
             apps.add(
                 DrawerApp(
-                    systemAppInfo.packageName,
-                    systemAppInfo.loadLabel(packageManager).toString(),
-                    systemAppInfo.loadIcon(packageManager)
+                    resolveInfo.activityInfo.packageName,
+                    resolveInfo.loadLabel(packageManager).toString(),
+                    resolveInfo.loadIcon(packageManager)
                 )
             )
         }
@@ -43,6 +46,5 @@ class AppDrawerRepository(private val packageManager: PackageManager) {
             sortedWith(compareBy(DrawerApp::label))
         })
     }
-
 
 }
